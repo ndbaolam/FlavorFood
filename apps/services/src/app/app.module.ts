096f35b8d912cf03b59ba2ee6af30a7194d0ba5b
ndbaolam <baolam782004@gmail.com>
2024-11-08 12:41:13 +0700

    feat: config, connect to database

--- apps/services/src/app/app.module.ts
+++ apps/services/src/app/app.module.ts
@@ -1,11 +1,37 @@
-import { Module } from '@nestjs/common';
-
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from '../config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Adjust path if needed
+        synchronize: true, // Set to false in production
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  onModuleInit() {
+    this.logger.log('Successfully connected to the database');
+  }
+}
