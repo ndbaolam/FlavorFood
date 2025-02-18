95c970f43751e09296a453d172f37ffec648e2e2
Minh Anh <minhanh@Minhs-MacBook-Air.local>
2025-02-18 20:36:53 +0700

    update call api

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -5,13 +5,8 @@ import SignUp from '../pages/Signup';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home';
 import Meals from '../pages/Meals';
-import BreakFast from '../pages/Meals/BreakFast';
-import Vegetarian from '../pages/Meals/Vegetarian';
-import Lunch from '../pages/Meals/Lunch';
-import Drinks from '../pages/Meals/Drinks';
-import Desserts from '../pages/Meals/Desserts';
 import Tips from '../pages/Tips';
-import RecipeDetail from '../pages/Meals/RecipeDetails';
+import RecipeDetail, { clientLoader as recipeLoader } from '../pages/Meals/RecipeDetails';
 import TipDetails from '../pages/Tips/TipDetails';
 import Profile from '../pages/Profile';
 import Favourite from '../pages/Favourite';
@@ -37,37 +32,11 @@ const routes: RouteObject[] = [
       {
         path: 'dish',
         element: <Meals />,
-        children: [
-          {
-            index: true,
-            element: <Meals />,
-          },
-          {
-            path: 'appetizer',
-            element: <BreakFast />,
-          },
-          {
-            path: 'main-course',
-            element: <Vegetarian />,
-          },
-          {
-            path: 'soup-hotpot',
-            element: <Lunch />,
-          },
-          {
-            path: 'drink',
-            element: <Drinks />,
-          },
-          {
-            path: 'dessert',
-            element: <Desserts />,
-          },
-          // General routes for individual recipe details within each category
-        ],
       },
       {
         path: 'dish/:slug.html',
         element: <RecipeDetail />,
+        loader: recipeLoader,
       }, //tips/asdfasdf.html
       {
         path: 'tips',
